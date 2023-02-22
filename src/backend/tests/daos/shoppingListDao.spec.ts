c3bec15fe496c9dd5a4fd9c76b9941f8cd16e163
mikmet <mikko.metsaranta@orangit.fi>
2023-02-22 20:29:51 +0000

    Refactor shopping list dao to implmenet base dao

--- src/backend/tests/daos/shoppingListDao.spec.ts
+++ src/backend/tests/daos/shoppingListDao.spec.ts
@@ -1,6 +1,6 @@
 import { ShoppingListDao } from "../../src/daos/shoppinListDao";
 import { describe, test, expect, beforeEach } from "@jest/globals";
-import ShoppingList from "../../src/domain/shoppingList.type";
+import ShoppingList from "../../src/domain/types/shoppingList.type";
 
 describe("shopping list dao", () => {
   describe("when empty", () => {
@@ -9,39 +9,39 @@ describe("shopping list dao", () => {
       sut = new ShoppingListDao();
     });
 
-    test("all() should return empty array", async () => {
-      const result = await sut.all();
+    test("loadAll() should return empty array", async () => {
+      const result = await sut.loadAll();
 
       expect(result).toStrictEqual([]);
     });
 
-    test("getById() should throw not found error", async () => {
+    test("get() should throw not found error", async () => {
       try {
-        await sut.getById("1");
+        await sut.get("1");
       } catch (e) {
         expect(e.message).toMatch("Not found");
       }
     });
 
-    test("insert() should add one shopping list and return it", async () => {
+    test("save() should add one shopping list and return it", async () => {
       const shoppingList = {
         name: "Test list",
         items: [],
       } satisfies ShoppingList;
-      const result = await sut.insert(shoppingList);
-      const allLists = await sut.all();
+      const result = await sut.save(shoppingList);
+      const allLists = await sut.loadAll();
 
       expect(allLists).toHaveLength(1);
       expect(result.name).toBe("Test list");
       expect(result.items).toHaveLength(0);
     });
 
-    test("insert() should add id to the list", async () => {
+    test("save() should add id to the list", async () => {
       const shoppingList = {
         name: "Test list",
         items: [],
       } satisfies ShoppingList;
-      const result = await sut.insert(shoppingList);
+      const result = await sut.save(shoppingList);
       expect(result.id).toBeDefined();
     });
 
@@ -63,11 +63,11 @@ describe("shopping list dao", () => {
         name: "Test list",
         items: [],
       } satisfies ShoppingList;
-      await sut.insert(shoppingList);
+      await sut.save(shoppingList);
     });
 
-    test("all() should return array with one list", async () => {
-      const result = await sut.all();
+    test("loadAll() should return array with one list", async () => {
+      const result = await sut.loadAll();
 
       expect(result).toHaveLength(1);
       expect(result[0].id).toBeDefined();
@@ -75,17 +75,17 @@ describe("shopping list dao", () => {
       expect(result[0].items).toHaveLength(0);
     });
 
-    test("getById() should throw Not found error when id doesn't exist", async () => {
+    test("get() should throw Not found error when id doesn't exist", async () => {
       try {
-        const result = await sut.getById("2");
+        const result = await sut.get("2");
         expect(true).toBe(false);
       } catch (e) {
         expect(e.message).toBe("Not found");
       }
     });
 
-    test("getById() should return list with given id", async () => {
-      const result = await sut.getById("1");
+    test("get() should return list with given id", async () => {
+      const result = await sut.get("1");
 
       expect(result.id).toBe("1");
       expect(result.name).toBe("Test list");
@@ -93,8 +93,8 @@ describe("shopping list dao", () => {
     });
 
     test("delete() should remove list and return it", async () => {
-      const result = await sut.delete("1");
-      const allLists = await sut.all();
+      const result = await sut.delete(await sut.get("1"));
+      const allLists = await sut.loadAll();
 
       expect(result.id).toBe("1");
       expect(result.name).toBe("Test list");
@@ -114,41 +114,41 @@ describe("shopping list dao", () => {
         name: "Test list",
         items: [],
       } satisfies ShoppingList;
-      await sut.insert(shoppingList1);
-      await sut.insert(shoppingList2);
+      await sut.save(shoppingList1);
+      await sut.save(shoppingList2);
     });
 
-    test("all() should return array with two lists", async () => {
-      const result = await sut.all();
+    test("loadAll() should return array with two lists", async () => {
+      const result = await sut.loadAll();
 
       expect(result).toHaveLength(2);
       expect(result[0].id).toBe("1");
       expect(result[1].id).toBe("2");
     });
 
-    test("getById() should return list with given id when there's many lists", async () => {
-      const result1 = await sut.getById("1");
-      const result2 = await sut.getById("2");
+    test("get() should return list with given id when there's many lists", async () => {
+      const result1 = await sut.get("1");
+      const result2 = await sut.get("2");
 
       expect(result1.id).toBe("1");
       expect(result2.id).toBe("2");
     });
 
-    test("insert() should not add duplicate ids", async () => {
-      await sut.delete("1");
-      await sut.insert({
+    test("save() should not add duplicate ids", async () => {
+      await sut.delete(await sut.get("1"));
+      await sut.save({
         name: "Test list",
         items: [],
       } satisfies ShoppingList);
-      const all = await sut.all();
+      const all = await sut.loadAll();
 
       expect(all[0].id).toBe("2");
       expect(all[1].id).not.toBe("2");
     });
 
     test("delete() should delete correct list", async () => {
-      const result = await sut.delete("1");
-      const allLists = await sut.all();
+      const result = await sut.delete(await sut.get("1"));
+      const allLists = await sut.loadAll();
 
       expect(result.id).toBe("1");
       expect(allLists).toHaveLength(1);
